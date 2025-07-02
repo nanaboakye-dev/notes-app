d3e4c0f9e2d35b07ae062a147df242320a3e093f
nanaboakye-dev <152764615+nanaboakye-dev@users.noreply.github.com>
2025-07-02 02:50:43 -0400

    prepared for render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,41 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from './config/db.js';
-import rateLimiter from './middleware/rateLimiter.js';
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// Enable CORS for frontend
-app.use(cors({ origin: "http://localhost:5173" }));
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Apply rate limiting to all requests
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// Notes API routes
 app.use("/api/notes", notesRoutes);
 
-// Connect to the database, then start the server
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log('Server is running on port:', PORT);
-    });
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
